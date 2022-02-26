76c599381d9f25cc1b9bdd0a0a1479fdb15d589a
sadumankucuk <sadumankucuk@gmail.com>
2022-02-26 12:34:14 +0300

    write Home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input
+        id="todo-input"
+        type="text"
+        v-model="task"
+        placeholder="add task"
+    />
+    <button id="add-todo">Add</button>
+    <TodoList/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+
+import TodoList from "@/components/TodoList";
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      task: ''
+    }
+  },
+  components: {TodoList},
 }
 </script>
