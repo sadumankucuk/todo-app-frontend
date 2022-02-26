5cafd01700fc496d589545d0226102d40305dd8c
sadumankucuk <sadumankucuk@gmail.com>
2022-02-26 22:26:36 +0300

    create api.js and add devServer proxy

---
+++ src/api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import adapter from "axios/lib/adapters/http";
+
+axios.defaults.adapter = adapter;
+
+export class API {
+    useProxy = false
+    constructor(url, useProxy) {
+        this.useProxy = useProxy
+
+        if(url === undefined || url === "") {
+            url = process.env.VUE_APP_BASE_API_URL;
+        }
+
+        if(url.endsWith("/")) {
+            url = url.substr(0, url.length -1)
+        }
+
+        this.url = url
+    }
+
+    withPath(path) {
+        if (this.useProxy) {
+            return path
+        }
+
+        if(!path.startsWith("/")) {
+            path = "/" + path
+        }
+        return `${this.url}${path}`
+    }
+
+    async addTodo(newTodo) {
+        return axios.post(this.withPath("api/v1/todos"), {
+            task: newTodo
+        }).then(r => r.data)
+    }
+
+    async getTodoList() {
+        return axios.get(this.withPath("api/v1/todos")).then(r => r.data)
+    }
+}
+
+export default new API(process.env.VUE_APP_BASE_API_URL, true);
